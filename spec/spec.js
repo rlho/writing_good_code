9a262c0ecf6431e4309286266c505f80caa04e55
riho.takagi <rihohearts@gmail.com>
2023-11-13 18:24:27 -0500

    Add test

---
+++ spec/spec.js
@@ -0,0 +1,11 @@
+describe("getRadian", function () {
+  it("return radian", function () {
+    expect(getRadian(1, 10, 10, 1)).toBe(-2.356194490192345);
+  });
+});
+
+describe("getRadian", function () {
+  it("return radian", function () {
+    expect(getRadian(100, 100, 2, 2)).toBe(-3.9269908169872414);
+  });
+});
